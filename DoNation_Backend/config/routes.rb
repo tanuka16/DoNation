da877e604dffb223abe6eedaf79aa8cbb581a70b
Tanuka16 <das.tanuka@yahoo.com>
2019-08-14 10:57:14 -0400

    mvp complete

--- DoNation_Backend/config/routes.rb
+++ DoNation_Backend/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :charities
   resources :donations
-  resources :users
+
+  post '/login', to: 'auth#create'
+  post '/signup', to: 'users#create'
+  get '/profile', to: 'users#profile'
 end
